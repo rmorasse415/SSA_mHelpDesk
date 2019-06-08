e4cf548b5533dbff241e347081769145c5c24d15
rmorasse415 <7054052+rmorasse415@users.noreply.github.com>
2019-06-08 18:21:58 -0400

    Update UI when LastUpdated property changes

--- SSA_mHelpDesk_XAML/SSA_mHelpDesk/Domain/MainWindowViewModel.cs
+++ SSA_mHelpDesk_XAML/SSA_mHelpDesk/Domain/MainWindowViewModel.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SSA_mHelpDesk.Domain
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : INotifyPropertyChanged
     {
-        public DateTime LastUpdated { get; set; } = DateTime.Now;
+        private DateTime _lastUpdated = DateTime.Now;
+        public DateTime LastUpdated { get => _lastUpdated;
+            set {
+                _lastUpdated = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
